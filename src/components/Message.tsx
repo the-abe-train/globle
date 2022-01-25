dc38dc2fc0c1b23f3990d8d7257fd6c4ff01d87c
the-abe-train <abraham.train@gmail.com>
2022-01-24 19:13:18 -0500

    Added interactivity notice

--- src/components/Message.tsx
+++ src/components/Message.tsx
@@ -3,9 +3,10 @@ import { answerName } from "../util/answer";
 type Props = {
   win: boolean;
   error: string;
+  firstGuess: boolean;
 };
 
-export function Message({ win, error }: Props) {
+export function Message({ win, error, firstGuess }: Props) {
   if (error) {
     return <p className="text-red-700 h-1">{error}</p>;
   } else if (win) {
@@ -14,6 +15,8 @@ export function Message({ win, error }: Props) {
         The Mystery Country is {answerName}!
       </p>
     );
+  } else if (firstGuess) {
+    return <p className="text-gray-700 h-1">Drag, click, and zoom-in on the globe to help you find your next guess</p>
   } else {
     return <p className="text-red-700 h-1"></p>;
   }
