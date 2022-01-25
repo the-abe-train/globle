8bc32b814aa70682b7abd847e59740f83fe7fade
the-abe-train <abraham.train@gmail.com>
2022-01-24 19:13:34 -0500

    Made settings look nice

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -1,21 +1,19 @@
 import React, { useContext, useState } from "react";
 import { ThemeContext } from "../context/ThemeContext";
 
-// TODO add cities version of the game
-
 function Toggle({ checked }: { checked: boolean }) {
   if (checked) {
     return (
       <div>
-        <div className="block bg-gray-100 w-14 h-8 rounded-full border-solid border-2 border-gray-500"></div>
+        <div className="block bg-gray-100 w-14 h-8 rounded-full border-2 border-gray-500"></div>
         <div className="dot absolute left-1 top-1 bg-blue-800 w-6 h-6 rounded-full transition"></div>
       </div>
     );
   } else {
     return (
       <div>
-        <div className="block bg-gray-600 w-14 h-8 rounded-full border-1 border-gray-500"></div>
-        <div className="dot absolute left-1 top-1 bg-blue-100 w-6 h-6 rounded-full transition translate-x-full"></div>
+        <div className="block bg-gray-100  w-14 h-8 rounded-full border-2 border-gray-500"></div>
+        <div className="dot absolute left-1 top-1 bg-blue-800 w-6 h-6 rounded-full transition translate-x-full"></div>
       </div>
     );
   }
@@ -23,12 +21,13 @@ function Toggle({ checked }: { checked: boolean }) {
 
 export default function Settings() {
   const themeContext = useContext(ThemeContext);
-  const [checked, setChecked] = useState(!themeContext.theme.nightMode);
+  const [themeToggle, setThemeToggle] = useState(!themeContext.theme.nightMode);
+  const [scopeToggle, setScopeToggle] = useState(true);
 
   function changeTheme(e: React.ChangeEvent<HTMLInputElement>) {
-    setChecked(e.currentTarget.checked);
+    setThemeToggle(e.currentTarget.checked);
     if (themeContext.setTheme) {
-      if (checked) {
+      if (themeToggle) {
         themeContext.setTheme({ nightMode: true });
       } else {
         themeContext.setTheme({ nightMode: false });
@@ -36,21 +35,52 @@ export default function Settings() {
     }
   }
 
+  function changeScope(e: React.ChangeEvent<HTMLInputElement>) {
+    setScopeToggle(e.currentTarget.checked);
+    // if (scopeToggle) {
+    //   if (themeToggle) {
+    //     scopeToggle({ nightMode: true });
+    //   } else {
+    //     scopeToggle({ nightMode: false });
+    //   }
+    // }
+  }
+
   return (
-    <div className="block mx-auto my-10 w-72 ">
+    <div className="flex-col space-y-8 mx-auto my-10 w-72 h-36">
       <div className="flex items-center justify-between">
-        <label htmlFor="toggleB" className="relative cursor-pointer">
+        <label htmlFor="theme" className="relative cursor-pointer">
           <input
+            id="theme"
             type="checkbox"
-            id="toggleB"
             className="sr-only"
-            checked={checked}
+            checked={themeToggle}
             onChange={changeTheme}
           />
-          <Toggle checked={checked} />
+          <Toggle checked={themeToggle} />
         </label>
-        <span className=" text-lg w-36">{checked ? "Day" : "Night"} Theme</span>
+        <span className=" text-lg w-36">
+          {themeToggle ? "Day" : "Night"} Theme
+        </span>
       </div>
+      <div className="flex items-center justify-between">
+        <label htmlFor="scope" className="relative cursor-pointer">
+          <input
+            id="scope"
+            type="checkbox"
+            className="sr-only"
+            checked={scopeToggle}
+            onChange={changeScope}
+          />
+          <Toggle checked={scopeToggle} />
+        </label>
+        <span className=" text-lg w-36">
+          {scopeToggle ? "Countries" : "Cities"}
+        </span>
+      </div>
+      {!scopeToggle && (
+        <p className="text-red-700">Globle: Cities Edition coming soon!</p>
+      )}
     </div>
   );
 }
