8c3a53a978db9e70b52c6798f003fb17ea7fc5ec
the-abe-train <abraham.train@gmail.com>
2022-01-26 17:09:15 -0500

    Added TODO

--- src/App.tsx
+++ src/App.tsx
@@ -7,6 +7,8 @@ import Settings from "./components/Settings";
 import Statistics from "./components/Statistics";
 import { ThemeContext } from "./context/ThemeContext";
 
+// TODO add preview image when sharing
+
 function App() {
   // State
   const [screen, setScreen] = useState("Help");
