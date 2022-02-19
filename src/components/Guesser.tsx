1b390b9a7ae5f53a3349f401341a1dd489992fb4
Dietrich <dietrich.anaya97@outlook.com>
2022-02-19 12:42:10 -0800

    auto suggest

--- src/components/Guesser.tsx
+++ src/components/Guesser.tsx
@@ -5,6 +5,8 @@ import { Message } from "./Message";
 import { polygonDistance } from "../util/distance";
 import alternateNames from "../data/alternate_names.json";
 const countryData: Country[] = require("../data/country_data.json").features;
+const minGuessLength = 2;
+const suggestionLimit = 3;
 
 type Props = {
   guesses: Country[];
@@ -16,6 +18,24 @@ type Props = {
 export default function Guesser({ guesses, setGuesses, win, setWin }: Props) {
   const [guessName, setGuessName] = useState("");
   const [error, setError] = useState("");
+
+  function getSuggestions(value: string) {
+    const inputValue = value.trim().toLowerCase();
+    const inputLength = inputValue.length;
+    
+    if (inputLength >= minGuessLength) {
+      let suggestions: Country[] = [];
+      suggestions = countryData.filter(country => {
+        const countryName = country.properties.NAME.toLowerCase();
+        if (countryName !== inputValue) {
+          return countryName.slice(0, inputLength) === inputValue;
+        }
+        return [];
+      });
+      return suggestions.slice(0,suggestionLimit);
+    }
+    return [];
+  }
 
   function findCountry(countryName: string, list: Country[]) {
     return list.find((country) => {
@@ -76,14 +96,14 @@ export default function Guesser({ guesses, setGuesses, win, setWin }: Props) {
     <div className="mt-10 mb-6 block mx-auto text-center">
       <form
         onSubmit={addGuess}
-        className="w-80 flex space-x-4 mx-auto my-2 justify-center"
+        className="w-80 flex space-x-4 mx-auto my-2 justify-center flex-wrap"
       >
         <input
           className="shadow px-2 py-1 md:py-0
           text-gray-700 dark:bg-slate-300 focus:outline-none 
           focus:shadow-outline disabled:bg-slate-400
           border rounded disabled:border-slate-400
-          w-full"
+          w-full flex-1"
           type="text"
           name="guesser"
           id="guesser"
@@ -92,6 +112,7 @@ export default function Guesser({ guesses, setGuesses, win, setWin }: Props) {
           disabled={win}
           placeholder={guesses.length === 0 ? "Enter country name here" : ""}
         />
+        
         <button
           className="bg-blue-700 dark:bg-purple-800 hover:bg-blue-900 dark:hover:bg-purple-900 disabled:bg-blue-900  text-white 
           font-bold py-1 md:py-2 px-4 rounded focus:shadow-outline "
@@ -100,6 +121,23 @@ export default function Guesser({ guesses, setGuesses, win, setWin }: Props) {
         >
           Enter
         </button>
+
+        <div className="shadow px-2 py-0
+          text-gray-700 dark:bg-slate-300 focus:outline-none 
+          focus:shadow-outline disabled:bg-slate-400
+          rounded disabled:border-slate-400
+          w-full bg-white !mx-0"
+        >
+          {getSuggestions(guessName).map(country => {
+            return (
+              <div
+              className="text-left" 
+              key={country.properties.ADMIN}
+              onClick={() => {setGuessName(country.properties.NAME)}}
+              >{country.properties.NAME}</div>
+            )
+          })}
+        </div>
       </form>
       <Message win={win} error={error} guesses={guesses.length} />
     </div>
