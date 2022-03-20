795049cf4bc4b83c77a6c6d02f92679b77802a5a
the-abe-train <abraham.train@gmail.com>
2022-03-20 12:31:41 -0400

    Replace coffee emoji with svg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,13 +1,11 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-// import useCheckMobile from "../hooks/useCheckMobile";
 import { getPath } from "../util/svg";
-import { isMobile } from "react-device-detect";
 
 export default function Footer() {
   const iconWidth = 14;
   const { nightMode } = useContext(ThemeContext).theme;
-  // const isMobile = useCheckMobile();
+
   return (
     <footer className="pt-8 pb-4 text-xs flex items-end justify-between w-full">
       <span className="flex space-x-3">
@@ -33,18 +31,27 @@ export default function Footer() {
           </svg>
         </a>
       </span>
-      <p className="">
-        Enjoying the game? {isMobile && <br />}
-        <a
-          className="underline"
-          target="_blank"
-          rel="noreferrer"
-          href="https://www.buymeacoffee.com/theabetrain"
-        >
-          Buy me a coffee!
-        </a>{" "}
-        ☕
-      </p>
+      <div className="flex flex-col sm:flex-row justify-start">
+        <span>Enjoying the game?</span>
+        <span className="flex">
+          <a
+            className="underline mx-1"
+            target="_blank"
+            rel="noreferrer"
+            href="https://www.buymeacoffee.com/theabetrain"
+          >
+            Buy me a coffee!
+          </a>
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={iconWidth}
+            viewBox="0 0 24 24"
+            fill={nightMode ? "rgb(209 213 219)" : "rgb(17 24 39)"}
+          >
+            <path d={getPath("coffee")} />
+          </svg>
+        </span>
+      </div>
     </footer>
   );
 }
