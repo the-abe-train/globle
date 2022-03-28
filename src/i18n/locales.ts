74125b3b79da5ddd25166bb0dc53579f2d57ef02
Israel77 <dev.israel.batista@gmail.com>
2022-03-28 20:07:41 -0300

    Added Portuguese as an available language on the TypeScript settings 🇵🇹🇧🇷

--- src/i18n/locales.ts
+++ src/i18n/locales.ts
@@ -2,6 +2,7 @@ import { LanguageName } from "../lib/country";
 import { Locale, LocaleMessages } from "../lib/locale";
 import { English } from "./messages/en-CA";
 import { Spanish } from "./messages/es-MX";
+import { Portuguese } from "./messages/pt-BR";
 
 // export const LOCALES: LocaleList = {
 //   English: "English",
@@ -11,9 +12,11 @@ import { Spanish } from "./messages/es-MX";
 export const LOCALES = {
   English: English,
   Spanish: Spanish,
+  Portuguese: Portuguese,
 };
 
 export const langNameMap: Record<Locale, LanguageName> = {
   "es-MX": "NAME_ES",
   "en-CA": "NAME_EN",
+  "pt-BR": "NAME_PT",
 };
--- src/i18n/messages/index.ts
+++ src/i18n/messages/index.ts
@@ -1,10 +1,12 @@
 import { LocaleMessages } from "../../lib/locale";
 import { English } from "./en-CA";
 import { Spanish } from "./es-MX";
+import { Portuguese } from "./pt-BR"
 
 const localeList: LocaleMessages = {
   "en-CA": English,
   "es-MX": Spanish,
+  "pt-BR": Portuguese,
 };
 
 export default localeList;
