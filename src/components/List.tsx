e1e35954bcf066c911a2f5fc19e06820321e943f
MimirPatapolov <mimirpatapolov@gmail.com>
2022-04-13 03:52:59 +0200

    Update List.tsx

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -32,6 +32,7 @@ export default function List({ guesses, win, globeRef }: Props) {
   const langNameMap: Record<Locale, LanguageName> = {
     "es-MX": "NAME_ES",
     "en-CA": "NAME_EN",
+    "fr-FR": "NAME_FR",
   };
   const langName = langNameMap[locale];
 
