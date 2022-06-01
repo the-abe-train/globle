590da37a3dd4f8ce546d050a1fe90c956f1bc278
the-abe-train <abraham.train@gmail.com>
2022-05-31 21:11:25 -0400

    Updated FAQ about distance calculations

--- src/i18n/messages/en-CA.ts
+++ src/i18n/messages/en-CA.ts
@@ -23,7 +23,7 @@ export const English: Messages = {
   Loading: "Loading...",
   FAQTitle: "FAQ",
   q1: "1. How is the distance between the answer and my guess calculated?",
-  a1: "Distance between countries is defined as the minimum distance between their borders along the Earth's surface.",
+  a1: "Distance between countries is defined as the minimum distance between their land borders along the Earth's surface. Territories (areas that appear in gray when their parent country is guessed) and water borders are not included in distance calculations.",
   q2: "2. How can I play the game if I am colour blind or visually impaired?",
   a2: "A high-contrast Colour Blind mode can be activated in <button>Settings</button>.",
   q3: "3. How does the game decide what is a valid country?",
