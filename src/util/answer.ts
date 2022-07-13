9c394a7cb4bcdd8b7d18cbba1c08c5613e29ff53
the-abe-train <abraham.train@gmail.com>
2022-07-12 21:52:33 -0400

    Reshuffling August first

--- src/util/answer.ts
+++ src/util/answer.ts
@@ -7,11 +7,14 @@ countryData.sort((a, b) => {
   return a.properties.FLAG[1].localeCompare(b.properties.FLAG[1]);
 });
 
+const shuffleAdjust = today < "2022-08-01" ? "5" : "6";
+
 function generateKeyNew(list: any[], day: string) {
   const [year, month, date] = day.split("-");
   const dayCode = Date.UTC(parseInt(year), parseInt(month) - 1, parseInt(date));
   const SHUFFLE_KEY = process.env.REACT_APP_SHUFFLE_KEY || "1";
-  const key = Math.floor(dayCode / parseInt(SHUFFLE_KEY + "5")) % list.length;
+  const key =
+    Math.floor(dayCode / parseInt(SHUFFLE_KEY + shuffleAdjust)) % list.length;
   return key;
 }
 
